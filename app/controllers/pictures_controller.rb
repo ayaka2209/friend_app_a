944b910bac15fd9ca5518501f8c48866cb7fa193
ayaka tooya <na081543@icloud.com>
2022-10-09 21:55:11 +0900

    pictures一覧表示機能作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,8 +1,20 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def new
     @picture = Picture.new
+  end
+
+  def create
+    Picture.create(picture_params)
+    redirect_to new_picture_path 
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image, :content, :user)
   end
 end
