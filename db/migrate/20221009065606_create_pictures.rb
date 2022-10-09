ea9bda800c5ed28cdf650bb2c0ce82164b1f52b6
ayaka tooya <na081543@icloud.com>
2022-10-09 16:42:40 +0900

    pictureモデル作成

--- db/migrate/20221009065606_create_pictures.rb
+++ db/migrate/20221009065606_create_pictures.rb
@@ -1,7 +1,9 @@
 class CreatePictures < ActiveRecord::Migration[6.1]
   def change
     create_table :pictures do |t|
-
+      t.text :image
+      t.text :content
+      t.references :user
       t.timestamps
     end
   end
